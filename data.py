be259cc1fd833efb8f53a1e8b79bfc47d447e851
Muhammad Hussain Javed <k213584@nu.edu.pk>
2024-04-24 21:29:13 +0500

    Updated data code

--- data.py
+++ data.py
@@ -16,10 +16,12 @@ import signal
 class PacketAnalysis:
     def __init__(self):
         self.columns = [
-            'dport', 'sport','protocol', 'tcp_flags', 'time bw prev packet' ,'pkt_len','ttl', 'payload size', 'label'
+            'dport', 'sport','protocol', 'flags', 'time bw prev packet' ,'pkt_len','ttl', 'payload size', 'label'
         ]
         self.packet_info = []
         self.prev_packet_time = 0
+        self.label = ''
+        self.file_name = ''
         signal.signal(signal.SIGINT, self._save_data_csv)
 
     def _save_data_csv(self, sig, frame):
@@ -37,14 +39,13 @@ class PacketAnalysis:
         if IP in packet:
             if TCP in packet:
                 # print(ls(packet[TCP]))
-                print(f"src ip: {packet[TCP].time}")
                 # print(proto_field.i2s[packet.proto])
                 # print(f"Payload size: {sys.getsizeof(bytes(packet[TCP].payload))}")
                 # Save TCP flag data 
                 current_packet_time = packet[TCP].time
                 current_packet_info = {}
                 current_packet_info[self.columns[0]] = packet[TCP].dport
-                current_packet_info[self.columns[1]] = packet[TCP].dport
+                current_packet_info[self.columns[1]] = packet[TCP].sport
                 current_packet_info[self.columns[2]] = 'tcp'
                 current_packet_info[self.columns[3]] = str(packet[TCP].flags)
                 current_packet_info[self.columns[4]] = current_packet_time - self.prev_packet_time
@@ -52,9 +53,24 @@ class PacketAnalysis:
                 current_packet_info[self.columns[5]] = len(packet[TCP].payload)
                 current_packet_info[self.columns[6]] = packet[IP].ttl
                 current_packet_info[self.columns[7]] = sys.getsizeof(packet[TCP].payload)
-                current_packet_info[self.columns[8]] = 'dos'
+                current_packet_info[self.columns[8]] = self.label
                 self.packet_info.append(current_packet_info)
                 pprint(current_packet_info)
+
+            elif UDP in packet:
+                current_packet_time = packet[UDP].time
+                current_packet_info = {}
+                current_packet_info[self.columns[0]] = packet[UDP].sport
+                current_packet_info[self.columns[1]] = packet[UDP].dport
+                current_packet_info[self.columns[2]] = packet[IP].proto
+                current_packet_info[self.columns[3]] = None
+                current_packet_info[self.columns[4]] = current_packet_time - self.prev_packet_time
+                self.prev_packet_time = current_packet_time
+                current_packet_info[self.columns[5]] = len(packet[UDP].payload)
+                current_packet_info[self.columns[6]] = packet[UDP].ttl
+                current_packet_info[self.columns[7]] = sys.getsizeof(packet[UDP].payload)
+                current_packet_info[self.columns[8]] = self.label
+
 
             elif ICMP in packet:
                 # print(binascii.hexlify(bytes(packet[ICMP].payload)))
@@ -64,11 +80,14 @@ class PacketAnalysis:
 
 
     def start_sniffer(self):
-        print("start?")
-        _ = input("")
+        self.label = input("label: ")
+        self.file_name = input("dataset_filename.csv", index=False)
+        count = input('total packets to sniff: ')
+        filter = input('filter to apply: ')
         print('sniffing started')
-        sniff(iface='Realtek Gaming GbE Family Controller', 
-                       prn=self._packet_analysis, filter='src host 192.168.1.123')
+        sniff(iface='Realtek RTL8852BE WiFi 6 802.11ax PCIe Adapter', 
+                       prn=self._packet_analysis, filter=filter, count=int(count))
+        
 
 
 sniffer = PacketAnalysis()
