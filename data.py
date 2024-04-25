961400cba4427505e810198eefe18389eac9fe96
Muhammad Hussain Javed <k213584@nu.edu.pk>
2024-04-25 16:04:16 +0500

    Added some more features in code

--- data.py
+++ data.py
@@ -9,85 +9,99 @@
 from scapy.all import *
 import pandas as pd
 import sys
-#import binascii
 from pprint import pprint
 import signal
+
+NETWORK_INTERFACE='Realtek Gaming GbE Family Controller'
 
 class PacketAnalysis:
     def __init__(self):
         self.columns = [
-            'dport', 'sport','protocol', 'flags', 'time bw prev packet' ,'pkt_len','ttl', 'payload size', 'label'
+            'dport', 'sport','protocol', 'flags', 'time bw prev packet','spkts','dpkts' ,'pkt_len','ttl', 'payload size', 'label'
         ]
         self.packet_info = []
         self.prev_packet_time = 0
         self.label = ''
         self.file_name = ''
+        self.sessions = [] # List of dictionaries to hold packets
+        self.local_pc_ip = get_if_addr(NETWORK_INTERFACE)
         signal.signal(signal.SIGINT, self._save_data_csv)
 
     def _save_data_csv(self, sig, frame):
         # Once ctrl+c pressed, save data to csv
         df = pd.DataFrame(self.packet_info)
         df.columns = self.columns
-        df.to_csv('ddos_dataset.csv',index=False)
+        df.to_csv(self.file_name,index=False)
         print('CTRL+C was used.')
         sys.exit(0)
-
-    # def _get_time_between(self, current_packet_time):
-
     
     def _packet_analysis(self, packet):
         if IP in packet:
             if TCP in packet:
-                # print(ls(packet[TCP]))
-                # print(proto_field.i2s[packet.proto])
-                # print(f"Payload size: {sys.getsizeof(bytes(packet[TCP].payload))}")
-                # Save TCP flag data 
                 current_packet_time = packet[TCP].time
                 current_packet_info = {}
+                # current_packet_info['src_ip'] = packet[IP].src
+                # current_packet_info['dst_ip'] = packet[IP].dst
                 current_packet_info[self.columns[0]] = packet[TCP].dport
                 current_packet_info[self.columns[1]] = packet[TCP].sport
-                current_packet_info[self.columns[2]] = 'tcp'
+                current_packet_info[self.columns[2]] = packet[IP].proto
                 current_packet_info[self.columns[3]] = str(packet[TCP].flags)
                 current_packet_info[self.columns[4]] = current_packet_time - self.prev_packet_time
                 self.prev_packet_time = current_packet_time
-                current_packet_info[self.columns[5]] = len(packet[TCP].payload)
-                current_packet_info[self.columns[6]] = packet[IP].ttl
-                current_packet_info[self.columns[7]] = sys.getsizeof(packet[TCP].payload)
-                current_packet_info[self.columns[8]] = self.label
+                if packet[IP].src == self.local_pc_ip:
+                    # Get the size of source to destination packets
+                    current_packet_info[self.columns[5]] = len(bytes(packet[TCP]))
+                    current_packet_info[self.columns[6]] = 0
+                elif packet[IP].src != self.local_pc_ip:
+                    # Get the size of dest to source packets
+                    current_packet_info[self.columns[5]] = 0
+                    current_packet_info[self.columns[6]] = len(bytes(packet[TCP]))
+                current_packet_info[self.columns[7]] = len(packet[TCP].payload)
+                current_packet_info[self.columns[8]] = packet[IP].ttl
+                current_packet_info[self.columns[9]] = sys.getsizeof(packet[TCP].payload)
+                current_packet_info[self.columns[10]] = self.label
                 self.packet_info.append(current_packet_info)
+                # if packet[IP].src not in self.sessions:
+                #     self.sessions.append({packet[IP].src: current_packet_info})
                 pprint(current_packet_info)
 
             elif UDP in packet:
                 current_packet_time = packet[UDP].time
                 current_packet_info = {}
+                # current_packet_info['src_ip'] = packet[IP].src
+                # current_packet_info['dst_ip'] = packet[IP].dst
                 current_packet_info[self.columns[0]] = packet[UDP].sport
                 current_packet_info[self.columns[1]] = packet[UDP].dport
                 current_packet_info[self.columns[2]] = packet[IP].proto
-                current_packet_info[self.columns[3]] = None
+                current_packet_info[self.columns[3]] = 0
                 current_packet_info[self.columns[4]] = current_packet_time - self.prev_packet_time
                 self.prev_packet_time = current_packet_time
-                current_packet_info[self.columns[5]] = len(packet[UDP].payload)
-                current_packet_info[self.columns[6]] = packet[UDP].ttl
-                current_packet_info[self.columns[7]] = sys.getsizeof(packet[UDP].payload)
-                current_packet_info[self.columns[8]] = self.label
-
-
-            elif ICMP in packet:
-                # print(binascii.hexlify(bytes(packet[ICMP].payload)))
-                print(f"ICMP timestamp: {packet[ICMP].time}")
-            # elif UDP in packet:
-            #     print(packet[UDP])
+                if packet[IP].src == self.local_pc_ip:
+                    # Get the size of source to destination packets
+                    current_packet_info[self.columns[5]] = len(bytes(packet[UDP]))
+                    current_packet_info[self.columns[6]] = 0
+                elif packet[IP].src != self.local_pc_ip:
+                    # Get the size of dest to source packets
+                    current_packet_info[self.columns[5]] = 0
+                    current_packet_info[self.columns[6]] = len(bytes(packet[UDP]))
+                current_packet_info[self.columns[7]] = len(packet[UDP].payload)
+                current_packet_info[self.columns[8]] = 0
+                current_packet_info[self.columns[9]] = sys.getsizeof(packet[UDP].payload)
+                current_packet_info[self.columns[10]] = self.label
+                self.packet_info.append(current_packet_info)
 
 
     def start_sniffer(self):
         self.label = input("label: ")
-        self.file_name = input("dataset_filename.csv", index=False)
+        self.file_name = input("File name: ")
         count = input('total packets to sniff: ')
         filter = input('filter to apply: ')
         print('sniffing started')
-        sniff(iface='Realtek RTL8852BE WiFi 6 802.11ax PCIe Adapter', 
+        sniff(iface=NETWORK_INTERFACE, 
                        prn=self._packet_analysis, filter=filter, count=int(count))
-        
+        df = pd.DataFrame(self.packet_info)
+        df.columns = self.columns
+        df.to_csv(self.file_name + '.csv', index=False)
 
 
 sniffer = PacketAnalysis()
