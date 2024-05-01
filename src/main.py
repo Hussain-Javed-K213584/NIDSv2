e2a15b1eff135d9558b19a39a74ecce653bea29b
Muhammad Hussain Javed <k213584@nu.edu.pk>
2024-05-01 21:45:17 +0500

    Merged code from model_live_test.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from tkinter import *
-from random import randint
 from scapy.all import *
-import multiprocessing
+from joblib import load
 import threading
+import os
+import pandas as pd
 
 sniffer_stop = False
 textbox = None
@@ -92,9 +93,112 @@ def gui_init():
         window.mainloop()
 
 class NIDS:
-    def __init__(self):
-         pass
-    
+     def __init__(self):
+          path = os.path.abspath('../model_training/dt_classifier.pkl')
+          self.model = load(path)
+          self.columns = [
+           'dport', 'sport','protocol', 'flags', 'time bw prev packet','spkts','dpkts' ,'pkt_len','ttl', 'payload size'
+          ]
+          self.packet_info = []
+          self.prev_packet_time = 0
+          self.label = ''
+          self.file_name = ''
+
+     def _flags_to_encode(self, tcp_flags: str) -> int:
+        """
+          Performs one-hot encoding on the tcp flags
+        """
+        if type(tcp_flags) != str:
+            tcp_flags = str(tcp_flags)
+        flag_mapping = {
+            'F': '8',
+            'S': '1',
+            'R': '2',
+            'P': '3',
+            'A': '4',
+            'U': '5',
+            'E': '6',
+            'C': '7',
+            '0': '0',
+            '9': '9'
+        }
+        list_of_flags = list()
+        try:
+            list_of_flags = list(tcp_flags)
+        except:
+            print(tcp_flags)
+        encoded_flag = ''
+        for flag in list_of_flags:
+            encoded_flag += flag_mapping[flag]
+        
+        return int(encoded_flag)
+
+     def _feature_extractor(self,packet):
+          """
+               Callback function passed to `prn` argument of scapy's
+               sniff function. This function extracts the relevent features for our model.
+          """
+          if IP in packet:
+            if TCP in packet:
+                current_packet_time = packet[TCP].time
+                current_packet_info = {}
+                # current_packet_info['src_ip'] = packet[IP].src
+                # current_packet_info['dst_ip'] = packet[IP].dst
+                current_packet_info[self.columns[0]] = packet[TCP].dport
+                current_packet_info[self.columns[1]] = packet[TCP].sport
+                current_packet_info[self.columns[2]] = packet[IP].proto
+                current_packet_info[self.columns[3]] = str(packet[TCP].flags)
+                current_packet_info[self.columns[4]] = current_packet_time - self.prev_packet_time
+                self.prev_packet_time = current_packet_time
+                if packet[IP].src == self.local_pc_ip:
+                    # Get the size of source to destination packets
+                    current_packet_info[self.columns[5]] = len(bytes(packet[TCP]))
+                    current_packet_info[self.columns[6]] = 0
+                elif packet[IP].src != self.local_pc_ip:
+                    # Get the size of dest to source packets
+                    current_packet_info[self.columns[5]] = 0
+                    current_packet_info[self.columns[6]] = len(bytes(packet[TCP]))
+                current_packet_info[self.columns[7]] = len(packet[TCP].payload)
+                current_packet_info[self.columns[8]] = packet[IP].ttl
+                current_packet_info[self.columns[9]] = sys.getsizeof(packet[TCP].payload)
+                self.packet_info.append(current_packet_info)
+
+            elif UDP in packet:
+                current_packet_time = packet[UDP].time
+                current_packet_info = {}
+                # current_packet_info['src_ip'] = packet[IP].src
+                # current_packet_info['dst_ip'] = packet[IP].dst
+                current_packet_info[self.columns[0]] = packet[UDP].sport
+                current_packet_info[self.columns[1]] = packet[UDP].dport
+                current_packet_info[self.columns[2]] = packet[IP].proto
+                current_packet_info[self.columns[3]] = 0
+                current_packet_info[self.columns[4]] = current_packet_time - self.prev_packet_time
+                self.prev_packet_time = current_packet_time
+                if packet[IP].src == self.local_pc_ip:
+                    # Get the size of source to destination packets
+                    current_packet_info[self.columns[5]] = len(bytes(packet[UDP]))
+                    current_packet_info[self.columns[6]] = 0
+                elif packet[IP].src != self.local_pc_ip:
+                    # Get the size of dest to source packets
+                    current_packet_info[self.columns[5]] = 0
+                    current_packet_info[self.columns[6]] = len(bytes(packet[UDP]))
+                current_packet_info[self.columns[7]] = len(packet[UDP].payload)
+                current_packet_info[self.columns[8]] = 0
+                current_packet_info[self.columns[9]] = sys.getsizeof(packet[UDP].payload)
+                self.packet_info.append(current_packet_info)
+
+            try:
+                # Predict the packet
+                current_packet_info['flags'] = self._flags_to_encode(
+                    tcp_flags=current_packet_info['flags']
+                )
+                df = pd.DataFrame([current_packet_info])
+                prediction = self.dt_model.predict(df)
+                if prediction != ['benign']:
+                    print(prediction)
+                    
+            except UnboundLocalError:
+                print('local var error')
 
 if __name__ == '__main__':
-    gui_init()
+    nids = NIDS()
