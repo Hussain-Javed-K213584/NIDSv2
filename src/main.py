b8183fb35dd35cb2fbebc1a25609a056f83a44f8
Muhammad Hussain Javed <k213584@nu.edu.pk>
2024-05-05 00:50:15 +0500

    Added dropdown menu for Windows

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import tkinter.scrolledtext as scrolledtext
 from tkinter import ttk
 import sv_ttk
 from scapy.all import *
+from scapy.arch.windows import get_windows_if_list
 from joblib import load
 import threading
 import os
@@ -18,6 +19,7 @@ import re
 sniffer_stop = False
 textbox = None
 sniffer_thread = None
+menu = None
 
 class NIDS:
     def __init__(self):
@@ -47,9 +49,18 @@ class NIDS:
           self.prev_packet_time = 0
           self.label = ''
           self.file_name = ''
-          self.local_pc_ip = get_if_addr('ens33')
           self.config_parser = configparser.ConfigParser()
           self.rule_dictionary = []
+          # Get interface list based on operating system
+          self.windows_if_list = None
+          self.linux_if_list = None
+          self.NETWORK_INTERFACE = None
+          self.local_pc_ip = get_if_addr(self.NETWORK_INTERFACE)
+          if system() == 'Windows':
+              self.windows_if_list = get_windows_if_list()
+              pprint(self.windows_if_list)
+          elif system() == 'Linux':
+              self.linux_if_list = get_if_list()
           logging.basicConfig(filename='alerts.log', filemode='a',format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 
@@ -120,6 +131,11 @@ class NIDS:
         """
         global sniffer_stop
         global sniffer_thread
+        global menu
+
+        # First thing is to set the correct network interface and get the correct host IP address
+        self.NETWORK_INTERFACE = menu.get().strip() # Strip removes the leading trailing whitespace I left for padding
+        self.local_pc_ip = get_if_addr(self.NETWORK_INTERFACE)
         self.rule_parser()
         start_button.config(state=DISABLED)
         if (sniffer_thread is None) or (not sniffer_thread.is_alive()):
@@ -135,22 +151,30 @@ class NIDS:
             respectively and kills the live sniffing process.
         """
         global sniffer_stop
+        global sniffer_thread
         stop_button.config(state=DISABLED)
         sniffer_stop = True
         start_button.config(state=ACTIVE)
         self.rule_dictionary.clear()
 
+    def _get_iface(self) -> str:
+        """
+            Returns the interface on each call. Used by scapy.sniff
+        """
+        return self.NETWORK_INTERFACE
+
     def _scapy_sniffer(self):
         global sniffer_stop
-        sniff(iface='ens33',prn=self._feature_extractor,stop_filter=self._stop_sniffing)
+        sniff(iface=self._get_iface(),prn=self._feature_extractor,stop_filter=self._stop_sniffing)
     
     def gui_init(self):
         """
             The function that start the GUI. Should be called in the end.
         """
         global textbox
+        global menu
         window = Tk()
-        window.geometry('600x480')
+        window.geometry('1024x768')
         window.title("NIDSv2")
         header_frame = ttk.Frame(window,width=200,height=400)
         header_frame.grid(row=0,column=0,columnspan=2, 
@@ -170,13 +194,15 @@ class NIDS:
         # textbox.config(state=DISABLED,yscrollcommand=text_scroll.set)
         # textbox.grid(row=2,column=0)
         # text_scroll.grid(row=2,column=1)
+
+        # This is responsible to display our logs
         textbox = scrolledtext.ScrolledText(left_frame,undo=True)
         textbox['font'] = ('consolas',12)
         textbox.config(state=DISABLED)
         textbox.grid(row=2,column=0)
         right_frame = ttk.Frame(window,width=100,height=200)
-        right_frame.grid(row=1,column=3)
-        # This is responsible to display our logs
+        right_frame.grid(row=2,column=5, padx=20,pady=20)
+        
         # Creating the start and stop buttons
         start_button = ttk.Button(right_frame,
                               text='Start NIDS',
@@ -188,12 +214,27 @@ class NIDS:
                                                     stop_button))
         stop_button.config(command=lambda: self._stopper(start_button,
                                                    stop_button))
-        start_button.grid()
-        stop_button.grid()
+        start_button.grid(padx=10,pady=10)
+        stop_button.grid(padx=10,pady=10)
+
+        bottom_frame = ttk.Frame(window, width=300,height=100)
+        bottom_frame.grid(row=3)
+        # Set the options for the menu
+        menu = StringVar()
+        menu.set("Select the network interface  ")
+        interface_options = []
+        if system() == 'Windows':
+            for dict in self.windows_if_list:
+                if dict['mac'] != '':
+                    interface_options.append(dict['description'] + '  ')
+
+        # Creating the dropdown menu
+        dropdown = ttk.OptionMenu(window, menu,*interface_options)
+        dropdown.grid()
 
         # Set theme only if OS is windows
         if system() == 'Windows':
-            sv_ttk.set_theme("light")
+            sv_ttk.set_theme("dark")
         window.mainloop()
 
     def _flags_to_encode(self, tcp_flags: str) -> int:
